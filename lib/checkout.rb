8b28b4c6338762ce21f3f84b9d61ed427521e923
Alex Johnson <info@alexjson.com>
2018-05-12 14:36:00 +0100

    Added some checkout tests

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,3 +1,11 @@
 class Checkout
 
+  attr_reader :scanned_items
+  attr_reader :promotions
+
+  def initialize(promotions = [])
+    @scanned_items = []
+    @promotions = promotions
+  end
+
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -2,20 +2,64 @@ require 'checkout'
 
 describe Checkout do
 
-  describe ".new" do
-    # 
-    # let(:promotion1) do
-    #   MultiBuyLineItemPromotion.new
-    # end
-
+  let(:line_item_promotion) do
+    MultiBuyLineItemPromotion.new('001', 2, -0.75)
   end
+
+  # let(:order_total_promotion) do
+  #   OrderTotalPromotion.new(60,10)
+  # end
+
+  let(:promotional_rules) do
+    [line_item_promotion]
+  end
+
+
+
+  describe ".new" do
+    context "when the checkout is initialized with promotions" do
+      let(:checkout) do
+        Checkout.new(promotional_rules)
+      end
+
+      it "should have no scanned items" do
+        expect(checkout.scanned_items).to eq []
+      end
+
+      it "should have no scanned items" do
+        expect(checkout.promotions).to be promotional_rules
+      end
+    end
+
+    context "when the checkout is initialized with no promotions" do
+      let(:checkout) do
+        Checkout.new
+      end
+
+      it "should have no scanned items" do
+        expect(checkout.scanned_items).to eq []
+      end
+
+      it "should have no scanned items" do
+        expect(checkout.promotions).to eq []
+      end
+    end
+  end #end .new
 
   describe "#scan" do
 
-  end
+    let(:checkout) do
+      Checkout.new
+    end
+
+    it "should add the item to the scanned_items array" do
+
+    end
+
+  end #end #scan
 
   describe "#total" do
 
-  end
+  end #end #total
 
 end
